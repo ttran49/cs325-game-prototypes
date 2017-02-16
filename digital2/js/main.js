e66e413eb7b049850b97509a0ceff0126d6763f3
Chuck Tran <ttran49@masonlive.gmu.edu>
2017-02-16 08:21:29 -0500

    WORKING WITHOUT MOVING

--- digital2/js/main.js
+++ digital2/js/main.js
@@ -1,101 +1,90 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create });
+window.onload = function() {
+"use strict";
 
-function preload(){
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+
+
+function preload() {
     game.load.image('pony', 'assets/pony.png');
     game.load.image('love','assets/love.png');
     game.load.image('bullet','assets/heart.png');
     game.load.image('ground','assets/ground.png');
 }
 
+var player;
 var platforms;
-var pony;
-var love;
-var bullet;
-var fireRate=100;
+var hearts;
+var fireRate=20;
 var nextFire=0;
-var score=0;
-var text;
+var bullets;
 
-function create(){
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.stage.backgroundColor = "#4488AA";
+function create() {
     
-    //create the ground
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     platforms = game.add.group();
     platforms.enableBody = true;
     var ground = platforms.create(0, game.world.height - 64, 'ground');
-    ground.scale.setTo(2, 2);
     ground.body.immovable = true;
-  
-    
-    //pony
-    pony = game.add.sprite(32, game.world.height - 150, 'pony');
-    game.physics.arcade.enable(pony);
-    pony.body.gravity.y = 300;
-    pony.body.collideWorldBounds = true;
 
-    
-    //heart aka love
-    love=game.add.group();
-    love.enableBody=true;
-    
-    //spawning hearts
-    for (var i=0;i<5;i++){
-        var heart=love.create(Math.random() * 800, Math.random(), 'love');
-        heart.body.gravity.y=40;
+
+    player = game.add.sprite(32, game.world.height - 150, 'pony');
+    game.physics.arcade.enable(player);
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(50, 'bullet');
+    bullets.setAll('checkWorldBounds', true);
+    bullets.setAll('outOfBoundsKill', true);
+
+    hearts = game.add.group();
+    hearts.enableBody = true;
+    for (var i = 0; i < 6; i++){
+        var heart = hearts.create(Math.random() * 800, Math.random(), 'love');
+        heart.body.gravity.y = Math.random() * 50;
     }
-    
-    //bullet
-    bullet = game.add.group();
-    bullet.createMultiple(50, 'bullet');
-    bullet.setAll('checkWorldBounds', true); 
-    bullet.setAll('outOfBoundsKill', true); 
-    
-    game.input.mouse.capture = true;
-    text = game.add.text(15, 10, "Score: 0", { font: "25px Arial" ,align: "left" });
 }
 
-function update(){
-    //check fire
-    if( game.input.activePointer.leftButton.isDown){
+function update() {
+    game.physics.arcade.collide(player, platforms);
+    //hearts hit the ground
+    game.physics.arcade.collide(platforms, hearts, hitGround, null, this);
+    //bullet hit hearts
+    game.physics.arcade.overlap(bullets, hearts, hitLove, null, this);
+
+    if(game.input.activePointer.leftButton.isDown){
         fire();
     }
-    
-    game.physics.arcade.collide(pony, platforms);
-    //heart hit the ground
-    game.physics.arcade.collide(gameVar, love, hitGround, null, this);
-    //hit the heart
-    game.physics.arcade.collide(bullet, love, hitLove);
-    
+
 }
 
 function fire() {
-
-    if (game.time.now > nextFire && bullet.countDead() > 0) {
+    if (game.time.now > nextFire && bullets.countDead() > 0) {
 
         nextFire = game.time.now + fireRate;
-        var bullets = bullet.getFirstDead();
-        game.physics.arcade.moveToPointer(bullets, 1000);
+        var bullet = bullets.getFirstDead();
+        bullet.reset(player.x - 8, player.y - 8);
+        game.physics.arcade.moveToPointer(bullet, 1000);
     }
 }
-
-function hitGround(ground, heart){
-    //kill then spawn more
-    heart.kill();
-    for (var i =0; i<5;i++){
-        var heart=love.create(Math.random() * 800, Math.random(), 'love');
-        heart.body.gravity.y=40;
-    }
-}
-function hitLove(){
-    //destroy then spawn more
-    love.destroy();
-    for (var i=0;i<3;i++){
-        var heart=love.create(Math.random() * 800, Math.random(), 'love');
-        heart.body.gravity.y=40;
-    }
     
-    //score
-    score+=10;
-    text.setText("Score: " + score);
+function hitGround(ground, heart){
+    heart.kill();
+    //kill then spawn more
+    for (var i =0; i<2;i++){
+        var heart=hearts.create(Math.random() * 800, Math.random(), 'love');
+        heart.body.gravity.y=40;
+    }
+}
+function hitLove(projectile,target){
+    //destroy then spawn more
+    target.kill();
+    projectile.kill();
+    for (var i=0;i<1;i++){
+        var heart=hearts.create(Math.random() * 800, Math.random(), 'love');
+        heart.body.gravity.y=40;
+    }
+}
 }
